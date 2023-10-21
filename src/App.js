36e48552f93f8d001017bc6fae2ff326a0bba47e
IsmailAvayil <ismailavayil@gmail.com>
2023-10-21 16:19:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import UserListPage from './components/User List Page/UserListPage';
 import AppContextProvider from './contexts/AppContext';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {HashRouter, Route, Routes } from "react-router-dom";
 import AddNewUser from './components/AddNewUser/AddNewUser';
 import UpdateUserPage from './components/UpdateUserPage/UpdateUserPage';
 import UserDetailsPage from './components/UserDetailsPage/UserDetailsPage';
@@ -10,7 +10,7 @@ import UserDetailsPage from './components/UserDetailsPage/UserDetailsPage';
 function App() {
   return (
     <AppContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <div className="App" >
           <div style={{position:"absolute",top:"50%",left:"50%",transform:"translate(-50%,-50%)" }}>
             <Routes>
@@ -22,7 +22,7 @@ function App() {
             </Routes>
           </div>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </AppContextProvider>
 
 
